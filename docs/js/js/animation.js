e1512dd52057762d3fa978cd762a657c921cb847
martinciro <martinciro11@gmail.com>
2023-05-27 23:20:29 -0500

    Se actualiza contenido

--- docs/js/js/animation.js
+++ docs/js/js/animation.js
@@ -15,49 +15,112 @@ function page(ru) {
   xhr.send();
 }
 
-function go(){
+function go() {
   page('../screen/player/playerAtaq.html');
-  document.addEventListener('DOMContentLoaded', function() {
-    // Código para acceder al elemento "barraVida" y establecer su ancho
-    const barraVida = document.getElementById('vida');
-    console.log(barraVida);
-    barraVida.style.width = salJuga + '%';
-  });
 }
 
-var salJuga=100;
-var salEnemi=100;
+var salJuga = 100;
+var salEnemi = 100;
 var EscuJuga;
 var EscuEne;
 var AtaJuga;
-var AtaEne=(Math.floor(Math.random() * 3));
+var AtaEne;
+
+//funcion para reinciar la vida
+function ReV(vd) {
+  if (vd <= 0) {
+    console.log("El jugador a perdido")
+  } else if (vd >= 100) {
+    vd = 100;
+    console.log("Se reincia la vida del enemigo")
+  }
+}
+
+//vida del jugador despues de atacar
+// Inicializar la variable vida con un valor inicial
+let vida = 100;
+
+function atacar() {
+  // Obtener la barra de vida
+  const barraVida = document.getElementById('vida');
+  
+  // Reducir el nivel de vida en un valor fijo (por ejemplo, 10)
+  salEnemi;
+  
+  // Verificar si el nivel de vida es menor o igual a 0
+  if (salEnemi <= 0) {
+    // Si el nivel de vida es menor o igual a 0, establecer la barra de vida en 0
+    barraVida.style.width = '0%';
+    barraVida.classList.add('red');
+    barraVida.innerHTML = 'Has perdido el juego';
+  } else {
+    // Si el nivel de vida es mayor a 0, actualizar la barra de vida con el nuevo nivel
+    barraVida.style.width = `${salEnemi}%`;
+  }
+}
+//
+
+//
+
 
 //Vida enemigo
 function vidaEne(AtaJuga) {
-    EscuEne = (Math.floor(Math.random() * 3))
-    console.log(EscuEne)
-    console.log(AtaJuga)
-    const ataques = [
-      [10, 20, 0],   // Agua para vidaEne
-      [0, 10, 20],   // Fuego para vidaEne
-      [20, 0, 10]    // Electrico para vidaEne
-    ];
-    
-    saEne -= ataques[EscuEne][AtaJuga];
-    //if(AtaJuga=)
-    console.log(saEne)
-  }
+  EscuEne = (Math.floor(Math.random() * 3))
+  console.log(EscuEne)
+  console.log(AtaJuga)
+  const ataques = [
+    [10, 20, 0],   // Agua para vidaEne
+    [0, 10, 20],   // Fuego para vidaEne
+    [20, 0, 10]    // Electrico para vidaEne
+  ];
+
+  salEnemi -= ataques[EscuEne][AtaJuga];
+  ReV(salEnemi);
+  atacar()
+}
+
+//funcion para reemplazar el contenido de los div
+function reem() {
+  const existingGif = document.getElementById('pla');
+  const newGif = document.createElement('img'); // Crea un contenedor img
+  newGif.src = '../acces/Player/DefensaPlayer/DefPlayer.gif'; //Ruta del reemplazo
+  existingGif.innerHTML = '';//Se envia el reemplazo
+  existingGif.appendChild(newGif);
+  const duration = 3010; // Tiempo en milisegundos (3000 ms = 3 segundos)
+
+  //volver al contenido original
+  setTimeout(() => {
+    existingGif.innerHTML = '<img src="../../acces/Player/EsperandoAtaquePlayer/esperandoAtaquePlayer.gif" alt="Jugador" >';/// Ruta para el contenido original
+  }, duration);
+}
 
 //vida salud Jugador
-function vdSalJuga(EscuJuga){
-    //Agua
-    if (EscuJuga == AtaEne){
-        salJuga -= 10;
-        //return has sido atacado con agua
-    }else if (EscuJuga < AtaEne){
-        salJuga -= 20;
-    }else {
-        salJuga -= 0;
-    }
+function vdSalJuga(EscuJuga) {
+  reem();
+  ReV(salJuga);
+  AtaEne = (Math.floor(Math.random() * 3));
+  //Agua
+  if (EscuJuga == AtaEne) {
+    salJuga -= 10;
+    //return has sido atacado con agua
+  } else if (EscuJuga < AtaEne) {
+    salJuga -= 20;
+  } else {
+    salJuga -= 0;
+  }
+}
+
+function reset(variable) {
+  if (variable === 'salJuga') {
+    salJuga = 100;
+    barraVida = document.getElementById('vida');
+    barraVida.style.width = `${salJuga}%`;
+    console.log(salJuga)
+  } else if (variable === 'salEnemi') {
+    salEnemi = 100;
+    barraVida = document.getElementById('vida');
+    barraVida.style.width = `${salEnemi}%`;
+    console.log(salEnemi)
+  }
 }
 
