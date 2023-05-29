175aae293c405c86d6316a365b2505d34376ce1b
martinciro <martinciro11@gmail.com>
2023-05-29 14:58:51 -0500

    SE actualiza contenido

--- docs/js/js/animation.js
+++ docs/js/js/animation.js
@@ -17,9 +17,6 @@ function page(ru) {
 
 function go() {
   page('screen/player/playerAtaq.html');
-
-
-
 }
 
 var salJuga = 100;
@@ -28,73 +25,88 @@ var EscuJuga;
 var EscuEne;
 var AtaJuga;
 var AtaEne;
-
 //funcion para reinciar la vida
-function ReV(vd) {
-  if (vd <= 0) {
-    console.log("El jugador a perdido")
+//SalEnemi, Ene, acces/Enemi/deadEnemi/1.svg
+function ReV(vd, iD, reemplazo) {
+  if(vd<=0){
+    var mensajePerdida = document.getElementById('mensajePerdida');
+    reem(`${iD}`, `${reemplazo}`, `${reemplazo}`);
+    mensajePerdida.style.display = "block";
   } else if (vd >= 100) {
     vd = 100;
-    console.log("Se reincia la vida del enemigo")
+    console.log("Se reincia la vida")
   }
 }
 
-//vida del jugador despues de atacar
-// Inicializar la variable vida con un valor inicial
-let vida = 100;
-
-function atacar() {
+function atacar(iD, salu) {
   // Obtener la barra de vida
-  const barraVida = document.getElementById('vidaJ');
-  
-  // Reducir el nivel de vida en un valor fijo (por ejemplo, 10)
-  salEnemi;
-  
+  const barraVida = document.getElementById(`${iD}`);
   // Verificar si el nivel de vida es menor o igual a 0
-  if (salEnemi <= 0) {
+  if (salu <= 0) {
     // Si el nivel de vida es menor o igual a 0, establecer la barra de vida en 0
     barraVida.style.width = '0%';
     barraVida.classList.add('red');
-    barraVida.innerHTML = 'Has perdido el juego';
+    mensajePerdida.style.display = "block";
   } else {
     // Si el nivel de vida es mayor a 0, actualizar la barra de vida con el nuevo nivel
-    barraVida.style.width = `${salEnemi}%`;
+    barraVida.style.width = `${salu}%`;
   }
 }
-//
-
-//
-
 
 //Vida enemigo
 function vidaEne(AtaJuga) {
-  reem();
-  EscuEne = (Math.floor(Math.random() * 3))
-  console.log(EscuEne)
-  console.log(AtaJuga)
   const ataques = [
     [10, 20, 0],   // Agua para vidaEne
     [0, 10, 20],   // Fuego para vidaEne
     [20, 0, 10]    // Electrico para vidaEne
   ];
-
+  const ataJugaGifs = [
+    "acces/Player/AtaquePlayer/Agua/AtaquePlayerAgua.gif",
+    "acces/Player/AtaquePlayer/Fuego/AtaquePlayerFuego.gif",
+    "acces/Player/AtaquePlayer/Electrico/AtaquePlayerElectrico.gif"
+  ];
+  const escuEneGifs = [
+    "acces/Enemi/DefensaEnemigo/Agua/DefeAguaEnemi.gif",
+    "acces/Enemi/DefensaEnemigo/Fuego/DefeFuegoEnemi.gif",
+    "acces/Enemi/DefensaEnemigo/Electrico/DefeElectroEnemi.gif"
+  ];
+  
+  reem("pla", "acces/Player/DefensaPlayer/DefPlayer.gif", "acces/Player/EsperandoAtaquePlayer/esperandoAtaquePlayer.gif");
+  
+  const EscuEne = Math.floor(Math.random() * 3);
+  console.log("El enemigo se defiende con " + EscuEne + " y el jugador ataca con " 
+  + AtaJuga);
+  
+  if (AtaJuga >= 0 && AtaJuga < ataques.length) {
+    const ataqueJugador = ataques[AtaJuga];
+    const gifJugador = ataJugaGifs[AtaJuga];
+    reem("pla", gifJugador, "acces/Player/EsperandoAtaquePlayer/esperandoAtaquePlayer.gif");
+    console.log(`El jugador ha atacado con ${AtaJuga}`);
+  }
+  
+  if (EscuEne >= 0 && EscuEne < escuEneGifs.length) {
+    const gifEnemigo = escuEneGifs[EscuEne];
+    reem("Ene", gifEnemigo, "acces/Enemi/EsperandoAtaqueEnemigo/esperandoAtaqueEnem.gif");
+    console.log(`El enemigo se ha defendido con ${EscuEne}`);
+  }
   salEnemi -= ataques[EscuEne][AtaJuga];
-  ReV(salEnemi);
-  atacar()
+  ReV(salEnemi,"Ene","acces/Enemi/deadEnemi/1.svg");
+  atacar("vidaE", salEnemi)
 }
 
+
 //funcion para reemplazar el contenido de los div
-function reem() {
-  const existingGif = document.getElementById('pla');
+function reem(iD, reemplazo, rutActu) {
+  const existingGif = document.getElementById(`${iD}`);
   const newGif = document.createElement('img'); // Crea un contenedor img
-  newGif.src = 'acces/Player/DefensaPlayer/DefPlayer.gif'; //Ruta del reemplazo
+  newGif.src = `${reemplazo}`; //Ruta del reemplazo
   existingGif.innerHTML = '';//Se envia el reemplazo
   existingGif.appendChild(newGif);
   const duration = 3010; // Tiempo en milisegundos (3000 ms = 3 segundos)
 
   //volver al contenido original
   setTimeout(() => {
-    existingGif.innerHTML = '<img src="acces/Player/EsperandoAtaquePlayer/esperandoAtaquePlayer.gif" alt="Jugador" width="100%" >';/// Ruta para el contenido original
+    existingGif.innerHTML = "<img src='" + rutActu + "' alt='Jugador' width='100%' >";
   }, duration);
 }
 
